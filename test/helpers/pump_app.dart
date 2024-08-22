c974648b6bb78254a233cbe664cba7eb974ffc71
Yolo CMS <baraa.habib321@gmail.com>
2024-08-22 21:39:01 +0300

    add SplashPage widget test

--- test/helpers/pump_app.dart
+++ test/helpers/pump_app.dart
@@ -1,47 +1,26 @@
 import 'dart:async';
-import 'dart:developer';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:task_master/app/environment/app_environment.dart';
-import 'package:task_master/app/view/app.dart';
-import 'package:task_master/core/utils/device_info/device_info_utils.dart';
-import 'package:task_master/core/utils/package_info/package_info_utils.dart';
-import 'package:task_master/locator.dart';
 
-Future<void> testBootstrap({required FutureOr<void> Function() pumper, required AppEnvironment environment}) async {
-
-  FlutterError.onError = (details) {
-    log(details.exceptionAsString(), stackTrace: details.stack);
-  };
-  await runZonedGuarded<Future<void>>(
-        () async {
-      TestWidgetsFlutterBinding.ensureInitialized();
-      // HydratedBloc.storage = await HydratedStorage.build(
-      //   storageDirectory: kIsWeb
-      //       ? HydratedStorage.webStorageDirectory
-      //       : await getTemporaryDirectory(),
-      // );
-      // Initialize Locator and Utils
-      await Future.wait([
-        Locator.locateServices(environment: environment),
-        PackageInfoUtils.init(),
-        DeviceInfoUtils.init(),
-      ]);
-
-      await pumper.call();
-    },
-        (error, stackTrace) {
-      log(error.toString(), stackTrace: stackTrace);
-    },
-  );
-}
+import 'observer.dart';
 
 
 extension PumpApp on WidgetTester {
-  Future<void> pumpApp(Widget widget) async {
-    return pumpWidget(
-      App(),
+  Future<void> pumpTestWidget(Widget widget) async {
+    await pumpWidget(
+      MaterialApp(
+        navigatorObservers: [observer,],
+        home: Builder(
+          builder: (context) {
+            ScreenUtil.init(context, minTextAdapt: true,
+              designSize: const Size(375, 667),
+            );
+            return widget;
+          },
+        ),
+      ),
     );
   }
 }
--- test/splash/view/splash_page_test.dart
+++ test/splash/view/splash_page_test.dart
@@ -1,8 +1,11 @@
 
 
 import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:task_master/app/environment/development_environment.dart';
 import 'package:task_master/app/view/app.dart';
@@ -10,48 +13,54 @@ import 'package:task_master/feature/auth/login/login.dart';
 import 'package:task_master/feature/splash/splash.dart';
 
 import '../../helpers/helpers.dart';
+import '../../helpers/observer.dart';
+import '../../helpers/shared_mockes.dart';
 
 class MockSplashCubit extends MockCubit<SplashState> implements SplashCubit {}
 
 Future<void> main() async {
   group('SplashPage', () {
-    // late SplashCubit splashCubit;
-    //
-    // setUp(() {
-    //   splashCubit = MockSplashCubit();
-    // });
-    setUp(() async {
-      const channel = MethodChannel('plugins.flutter.io/path_provider');
-      TestDefaultBinaryMessengerBinding
-          .instance
-          .defaultBinaryMessenger
-          .setMockMethodCallHandler(channel, (call) async {
-          return '/home/user/directory'; // Return a valid path (e.g., current directory)
+    final splashCubit = MockSplashCubit();
+    final storage = MockTaskMasterStorage();
 
-      });
-      // TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(
-      //     MethodCall methodCall) async {
-      //   return '.'; // Return a valid path (e.g., current directory)
-      // });
+    setUp(() {
+
     });
 
-    test('user save data', () async {
-      final directory = await getApplicationDocumentsDirectory();
-      print(directory);
-      // Your test logic here
-      // ...
-    });
-    testWidgets('renders SplashPage', (tester) async {
-      await testBootstrap(
-          pumper: () async {
-            await tester.pumpApp(
-              App(),
-            );
-          },
-          environment: DevelopmentEnvironment(),
+    when(() => storage.getUserData).thenReturn({});
+
+    testWidgets('User already logged in', (tester) async {
+      when(() => splashCubit.state)
+          .thenReturn(const SplashState.authenticated());
+
+      await tester.pumpTestWidget(BlocProvider<SplashCubit>.value(
+        value: splashCubit,
+        child: const SplashView(),
+      ),
       );
 
-      expect(find.byType(LoginView), findsOneWidget);
+      final finder = find.byType(SplashView);
+
+      expect(finder, findsOneWidget);
+    },
+    );
+
+    testWidgets(
+      'User is not logged in and redirected to login view', (tester) async {
+      when(() => splashCubit.state)
+          .thenReturn(const SplashState.unauthenticated());
+
+      await tester.pumpTestWidget(
+        BlocProvider<SplashCubit>.value(
+          value: splashCubit,
+          child: const SplashView(),
+        ),
+      );
+
+      // final finder = find.byType(LoginView);
+      observer.onPushed = (Route<dynamic>? route, Route<dynamic>? previousRoute) {
+        expect(route is PageRoute && route.settings.name == 'SplashPage', isTrue);
+      };
     },
     );
   });
